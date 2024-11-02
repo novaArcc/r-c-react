e876812f914307009ef352a6fcafec47de10de34
anyagardner <anya@blisslandpizza.com>
2024-11-02 19:14:56 +0000

    r&c react start of homepage/footer/navbar finished

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './components/Navbar/Navbar.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
