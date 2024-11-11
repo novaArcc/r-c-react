f9ca9c9ec91524f8224919c65fc1721ca773e744
anyagardner <anya@blisslandpizza.com>
2024-11-11 21:20:46 +0000

    Added about page, not finished styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import { Carousel, Content, GetInTouch, RandomBeerFact } from "./pages/Home/Home";
+import About from "./pages/About/About";
 import './App.css';
 
 function App() {
   return (
+  <Router>
     <div className="App">
       <Navbar />
+
+    <Routes>
+
+     <Route path="/" element={
+      <>
       <Carousel />
       <Content />
       <GetInTouch />
       <RandomBeerFact />
+      </>
+     }/>
+
+      <Route path="/about" element={<About />} />
+
+    </Routes>
+
       <Footer />
     </div>
+    </Router>
   );
 }
 
