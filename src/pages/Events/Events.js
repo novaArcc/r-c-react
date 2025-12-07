be4a3006d195c7c37dcdea63401cd2624e95ebbb
anyagardner <anya@blisslandpizza.com>
2025-12-07 12:43:44 +0000

    events update

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -24,20 +24,6 @@ const EventsHero = () => {
 }
 
 const events = [
-      {
-    title: "Flockin' Killer Pool",
-    date: "Wednesday 26th November 20:00",
-    description: "Supported by our wonderful Friends over at BaaadFlockers. Memorise the Rules, come on down & enjoy a perfect mix of Chaos and Fun!",
-    image: killerPoolImg,
-    link: poolRules
-  },
-        {
-    title: "Flockin' Killer Pool",
-    date: "Wednesday 3rd December 20:00",
-    description: "Supported by our wonderful Friends over at BaaadFlockers. Memorise the Rules, come on down & enjoy a perfect mix of Chaos and Fun!",
-    image: killerPoolImg,
-    link: poolRules
-  },
           {
     title: "Flockin' Killer Pool",
     date: "Wednesday 10th December 20:00",
@@ -83,6 +69,20 @@ const events = [
                       {
     title: "Flockin' Killer Pool",
     date: "Wednesday 28th January 20:00",
+    description: "Supported by our wonderful Friends over at BaaadFlockers. Memorise the Rules, come on down & enjoy a perfect mix of Chaos and Fun!",
+    image: killerPoolImg,
+    link: poolRules
+  },
+                      {
+    title: "Flockin' Killer Pool",
+    date: "Wednesday 4th February 20:00",
+    description: "Supported by our wonderful Friends over at BaaadFlockers. Memorise the Rules, come on down & enjoy a perfect mix of Chaos and Fun!",
+    image: killerPoolImg,
+    link: poolRules
+  },
+                      {
+    title: "Flockin' Killer Pool",
+    date: "Wednesday 11th February 20:00",
     description: "Supported by our wonderful Friends over at BaaadFlockers. Memorise the Rules, come on down & enjoy a perfect mix of Chaos and Fun!",
     image: killerPoolImg,
     link: poolRules
